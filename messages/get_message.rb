5c52479cd8cf489086711520be19444db7644b5c
nerbyk <danil.maximov2000@gmail.com>
2020-06-01 18:44:21 +0200

    class to get messages from yaml

---
+++ messages/get_message.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+class GetMessageText
+  attr_reader :replies_list, :case_text, :client
+  def initialize(client:)
+    @client = client
+    @replies_list = YAML.safe_load(File.read('./messages/msg-examples/' + client + '_messages.yaml'))
+  end
+
+  def reply(case_text)
+    replies_list[case_text]
+  end
+end
