da4b7c118c8c74d956efac9729fb5636604fb314
nerbyk <danil.maximov2000@gmail.com>
2020-06-01 20:02:57 +0200

    DIP fix

--- bot_options.rb
+++ bot_options.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
-require './messages/get_message.rb'
+require 'singleton'
 
 class BotOptions
-  attr_accessor :bot, :message, :client
+  attr_accessor :bot, :message
   include Singleton
-  def initialize(client: 'config')
+  def initialize
     @bot = bot
-    @client = client
     @message = message
   end
 
   def send_message(text:, markup: nil)
-    bot.api.send_message(chat_id: message.from.id, text: GetMessageText.new(client: client).reply(text), reply_markup: markup)
+    bot.api.send_message(chat_id: message.from.id, text: text, reply_markup: markup)
   end
 
   def delete_markup
