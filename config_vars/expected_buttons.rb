d7044fde637431e24bb69b8b083ec9389bd15db0
nerbyk <danil.maximov2000@gmail.com>
2020-06-03 17:05:21 +0200

    new config files with constants

---
+++ config_vars/expected_buttons.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class BotButtons
+  ADD_ADMIN = 'Add Admin'
+  DELETE_ADMIN = 'Delete Admin'
+end
---
+++ config_vars/expected_commands.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class BotCommands
+  START = '/start'
+  MANAGE_ADMINS = '/manage_admins'
+end
