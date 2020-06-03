4be639019dce3d2ec90fa63e3254924409d71094
nerbyk <danil.maximov2000@gmail.com>
2020-06-03 03:16:39 +0200

    encoding added

--- messages/get_message.rb
+++ messages/get_message.rb
@@ -6,7 +6,7 @@ class GetMessageText
   attr_reader :replies_list, :case_text, :client
   def initialize(client:)
     @client = client
-    @replies_list = YAML.safe_load(File.read('./messages/msg-examples/' + client + '_messages.yml'))
+    @replies_list = YAML.safe_load(File.read('./messages/msg-examples/' + client + '_messages.yml', encoding: 'utf-8'))
   end
 
   def reply(case_text)
