0d4e9e36fe3825d454fbebec0b30baf16272c8e2
nerbyk <danil.maximov2000@gmail.com>
2020-06-03 02:02:05 +0200

    turned off exception handling to get detailed err

--- bot.rb
+++ bot.rb
@@ -5,6 +5,7 @@ require 'dotenv'
 require './bot_options.rb'
 require './messages/responder/responder.rb'
 require './messages/responder_buttons/responder.rb'
+require './messages/get_user_role.rb'
 
 Dotenv.load('./.env')
 
@@ -13,15 +14,15 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   bot_options.bot = bot # to get access to bot from everywhere without passing bot and message as argument any time
   bot.listen do |message|
     bot_options.message = message
-    begin
-      case message
-      when Telegram::Bot::Types::CallbackQuery
-        # ButtonResponder.new
-      else
-        MessageResponder.new(role: 'admin') # TODO: class to check role of user via DB to separate access levels
-      end
-    rescue StandardError => e
-      p e
+    # begin
+    case message
+    when Telegram::Bot::Types::CallbackQuery
+      # ButtonResponder.new
+    else
+      MessageResponder.new(role: GetUserRole.user_role) # TODO: class to check role of user via DB to separate access levels
     end
+    #  rescue StandardError => e
+    #    p e
+    #  end
   end
 end
