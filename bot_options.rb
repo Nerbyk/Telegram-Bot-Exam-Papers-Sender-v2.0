b09f5cb7d3d1a444c7ab4dc9bd856f4e8c1bda87
nerbyk <danil.maximov2000@gmail.com>
2020-06-03 17:04:44 +0200

    new core methods for manipulations with bot

--- bot_options.rb
+++ bot_options.rb
@@ -13,10 +13,36 @@ class BotOptions
   end
 
   def send_message(text:, markup: nil)
-    bot.api.send_message(chat_id: message.from.id, text: my_text.reply(text), reply_markup: markup)
+    bot.api.send_message(chat_id: message.from.id,
+                         text: my_text.reply(text),
+                         reply_markup: markup)
+  end
+
+  def delete_keyboard
+    bot.api.reply_keyboard_remove(remove_keyboard: true)
   end
 
   def delete_markup
-    bot.api.reply_keyboard_remove(remove_keyboard: true)
+    bot.api.edit_message_reply_markup(chat_id: message.from.id,
+                                      message_id: message.message.message_id)
+  end
+
+  def edit_message(text:, markup: nil)
+    bot.api.edit_message_text(chat_id: message.from.id,
+                              message_id: message.message.message_id,
+                              text: my_text.reply(text),
+                              reply_markup: markup)
+  rescue StandardError
+    send_message(text: 'admin_exepction_error')
+  end
+
+  def get_single_input
+    bot.listen do |message|
+      if message.text
+        return message.text
+      else
+        return false
+      end
+    end
   end
 end
