f405a429b0d381ebcdd599980318f28895afa1aa
nerbyk <danil.maximov2000@gmail.com>
2020-06-03 03:18:15 +0200

    assigning text examples obj according to role

--- messages/get_user_role.rb
+++ messages/get_user_role.rb
@@ -8,6 +8,7 @@ class GetUserRole
   def self.user_role
     role = UserConfigDb.instance.get_role
     BotOptions.instance.role = role
+    BotOptions.instance.my_text = GetMessageText.new(client: role.downcase)
     role
   end
 end
