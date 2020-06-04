8ae2579cb5b323864421aed10866b944e2e9739c
nerbyk <danil.maximov2000@gmail.com>
2020-06-04 19:57:36 +0200

    new constants for new buttons and commands

--- config_vars/config_vars.rb
+++ config_vars/config_vars.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 module CfgConst
-    class BotButtons
-        ADD_ADMIN = 'Add Admin'
-        DELETE_ADMIN = 'Delete Admin'
-    end
+  class BotButtons
+    ADD_ADMIN = 'Add Admin'
+    DELETE_ADMIN = 'Delete Admin'
+    ADD_SUBJECT = 'Add Subject'
+    EDIT_SUBJECT = 'Delete Subject'
+  end
 
-    
-    class BotCommands
-        START = '/start'
-        MANAGE_ADMINS = '/manage_admins'
-    end
+  class BotCommands
+    START = '/start'
+    MANAGE_ADMINS = '/manage_admins'
+    UPDATE_DOCUMENTS = '/update_documents'
+  end
 
-    class Roles
-        ADMIN = 'admin'
-        USER  = 'user'
-        DEV   = 'dev'
-        MODERATOR = 'moderator'
-    end     
+  class Roles
+    ADMIN = 'admin'
+    USER  = 'user'
+    DEV   = 'dev'
+    MODERATOR = 'moderator'
+  end
 end
