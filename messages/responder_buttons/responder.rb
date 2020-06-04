6f3a28cb25173d67e338d0c5acd675b47859e1fa
nerbyk <danil.maximov2000@gmail.com>
2020-06-04 14:36:54 +0200

    updated to new config module

--- messages/responder_buttons/responder.rb
+++ messages/responder_buttons/responder.rb
@@ -3,7 +3,6 @@
 require './messages/responder_buttons/invoker.rb'
 require './messages/responder_buttons/receiver.rb'
 require './messages/responder_buttons/buttons.rb'
-require './config_vars/expected_buttons.rb'
 class ButtonResponder
   def initialize
     @receiver = ButtonReceiver.new
@@ -13,8 +12,8 @@ class ButtonResponder
 
   def respond
     case BotOptions.instance.message.data
-    when BotButtons::ADD_ADMIN then @invoker.execute(AddAdminButton.new(@receiver))
-    when BotButtons::DELETE_ADMIN then @invoker.execute(DeleteAdminButton.new(@receiver))
+    when  CfgConst::BotButtons::ADD_ADMIN then @invoker.execute(AddAdminButton.new(@receiver))
+    when  CfgConst::BotButtons::DELETE_ADMIN then @invoker.execute(DeleteAdminButton.new(@receiver))
     end
   end
 end
