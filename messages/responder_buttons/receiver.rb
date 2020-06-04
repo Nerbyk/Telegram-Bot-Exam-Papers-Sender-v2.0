2c360976525d5bf149f5fe4b9fbab1984b247995
nerbyk <danil.maximov2000@gmail.com>
2020-06-04 14:36:40 +0200

    delete_admin decompozed

--- messages/responder_buttons/receiver.rb
+++ messages/responder_buttons/receiver.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ButtonReceiver
+  # admin buttons
   def add_admin
     BotOptions.instance.edit_message(text: 'manage_admins_add')
     begin
@@ -12,19 +13,13 @@ class ButtonReceiver
       BotOptions.instance.send_message(text: 'manage_admins_error')
     else
       UserConfigDb.instance.add_admin(input: input)
+      Invoker.new.execute(StartCommand.new(Receiver.new))
     end
   end
 
   def delete_admin
-    array_wtih_admins = UserConfigDb.instance.get_admins
-    display_string = ' '
-    array_for_inline = []
-    array_wtih_admins.each do |hash|
-      display_string += "\n\n user id = #{hash[:user_id]} | #{hash[:user_name]}"
-      array_for_inline << [hash[:user_id]]
-    end
-    p(array_for_inline)
-    markup = MakeInlineMarkup.new(*array_for_inline).get_board(one_time_keyboard: true)
+    display_string, array_for_inline, markup = get_list_of_admins
+    BotOptions.instance.delete_markup
     BotOptions.instance.send_message(text: 'manage_admins_delete', markup: markup, additional_text: display_string)
     to_delete = BotOptions.instance.get_single_input
     if array_for_inline.flatten.include?(to_delete)
@@ -34,5 +29,24 @@ class ButtonReceiver
     else
       BotOptions.instance.send_message(text: 'manage_admins_error')
     end
+    # TODO: After including inspect_request block
+    # change state, if a request is inspecting by this admin
+    # while deleting permissions + send notification about rights
+  end
+
+  private
+
+  def get_list_of_admins
+    array_wtih_admins = UserConfigDb.instance.get_admins
+    display_string = ' '
+    array_for_inline = []
+    array_wtih_admins.each do |hash|
+      p hash[:user_id]
+      display_string += "\n\n user id = #{hash[:user_id]} | #{hash[:user_name]}"
+      array_for_inline << [hash[:user_id]]
+    end
+    p array_for_inline
+    markup = MakeInlineMarkup.new(*array_for_inline).get_board
+    [display_string, array_for_inline, markup]
   end
 end
