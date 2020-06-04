87bfe31d025d6700c33df2974078a4ad34f19948
nerbyk <danil.maximov2000@gmail.com>
2020-06-04 16:08:27 +0200

    some blocks moved to new class

--- messages/responder_buttons/receiver.rb
+++ messages/responder_buttons/receiver.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
+require './messages/responder_buttons/receiver_helper.rb'
+
 class ButtonReceiver
   # admin buttons
   def add_admin
     BotOptions.instance.edit_message(text: 'manage_admins_add')
     begin
       input = BotOptions.instance.get_single_input.split(' ')
-      raise if input.length != 2
-      raise unless input.first.to_i.is_a? Integer
-      raise unless input.last.is_a? String
+      ReceiverHelper.check_add_admin_input(input) ? true : raise
     rescue StandardError
       BotOptions.instance.send_message(text: 'manage_admins_error')
     else
@@ -18,7 +18,7 @@ class ButtonReceiver
   end
 
   def delete_admin
-    display_string, array_for_inline, markup = get_list_of_admins
+    display_string, array_for_inline, markup = ReceiverHelper.get_list_of_admins
     BotOptions.instance.delete_markup
     BotOptions.instance.send_message(text: 'manage_admins_delete', markup: markup, additional_text: display_string)
     to_delete = BotOptions.instance.get_single_input
@@ -32,21 +32,5 @@ class ButtonReceiver
     # TODO: After including inspect_request block
     # change state, if a request is inspecting by this admin
     # while deleting permissions + send notification about rights
-  end
-
-  private
-
-  def get_list_of_admins
-    array_wtih_admins = UserConfigDb.instance.get_admins
-    display_string = ' '
-    array_for_inline = []
-    array_wtih_admins.each do |hash|
-      p hash[:user_id]
-      display_string += "\n\n user id = #{hash[:user_id]} | #{hash[:user_name]}"
-      array_for_inline << [hash[:user_id]]
-    end
-    p array_for_inline
-    markup = MakeInlineMarkup.new(*array_for_inline).get_board
-    [display_string, array_for_inline, markup]
   end
 end
