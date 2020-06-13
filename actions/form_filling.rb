28f97e62141f900eb3a62570be6ff4ed7fba91a2
nerbyk <danil.maximov2000@gmail.com>
2020-06-13 18:03:55 +0200

    exception button push class

---
+++ actions/form_filling.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Form
+  def start
+    UserConfigDb.instance.get_role # to initialize new user in DB
+    send(UserConfigDb.instance.get_status)
+  end
+
+  private
+
+  def logged; end
+
+  def name_step; end
+
+  def link_step; end
+
+  def subjects_step; end
+
+  def photo_step; end
+
+  def in_queue; end
+
+  def accepted; end
+
+  def banned; end
+end
