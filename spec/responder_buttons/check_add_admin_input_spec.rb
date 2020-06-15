fc0724980b03d20fe05d41d86a572462afe5a46b
nerbyk <danil.maximov2000@gmail.com>
2020-06-15 18:59:20 +0200

    updated (class name was changed)

--- spec/responder_buttons/check_add_admin_input_spec.rb
+++ spec/responder_buttons/check_add_admin_input_spec.rb
@@ -2,30 +2,30 @@
 
 require './messages/responder_buttons/receiver_helper.rb'
 
-describe ReceiverHelper do
-  describe '.check_add_admin_input' do
+describe ReceiverButtonHelper do
+  describe '.check_db_string' do
     context 'array of strings' do
       context "given ['1234', 'nameSurname']" do
         it 'returns true' do
-          expect(ReceiverHelper.check_add_admin_input(%w[1234 nameSurname])).to eq(true)
+          expect(ReceiverButtonHelper.check_db_string(%w[1234 nameSurname])).to eq(true)
         end
       end
 
       context "given ['nameSurname', '1234']" do
         it 'returns false' do
-          expect(ReceiverHelper.check_add_admin_input(%w[nameSurname 1234])).to eq(false)
+          expect(ReceiverButtonHelper.check_db_string(%w[nameSurname 1234])).to eq(false)
         end
       end
 
       context "given ['nameSurname', '1234', 'oneMoreItem']" do
         it 'returns false' do
-          expect(ReceiverHelper.check_add_admin_input(%w[nameSurname 1234 oneMoreItem])).to eq(false)
+          expect(ReceiverButtonHelper.check_db_string(%w[nameSurname 1234 oneMoreItem])).to eq(false)
         end
       end
 
       context 'given [1234]' do
         it 'returns false' do
-          expect(ReceiverHelper.check_add_admin_input([1234])).to eq(false)
+          expect(ReceiverButtonHelper.check_db_string([1234])).to eq(false)
         end
       end
     end
