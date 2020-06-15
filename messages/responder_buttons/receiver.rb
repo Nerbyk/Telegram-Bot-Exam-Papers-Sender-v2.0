9754f9079b01e0370d2d8d2f052c59c2c43c00f4
nerbyk <danil.maximov2000@gmail.com>
2020-06-15 18:56:36 +0200

    exception handling for add_admin button

--- messages/responder_buttons/receiver.rb
+++ messages/responder_buttons/receiver.rb
@@ -7,12 +7,14 @@ class ButtonReceiver
   def add_admin
     BotOptions.instance.edit_message(text: 'manage_admins_add')
     begin
-      input = BotOptions.instance.get_single_input.text.split(' ')
-      ReceiverButtonHelper.check_db_string(input) ? true : raise
-    rescue StandardError
+      input = BotOptions.instance.get_single_input
+      text = input.text.split(' ')
+      ReceiverButtonHelper.check_db_string(text) ? true : raise('Input check failed')
+    rescue Exception => e
+      ErrorLogDb.instance.log_error(level: inspect + '=>' + caller[0][/`.*'/][1..-2], message: input, exception: e.inspect)
       BotOptions.instance.send_message(text: 'manage_admins_error')
     else
-      UserConfigDb.instance.add_admin(input: input)
+      UserConfigDb.instance.add_admin(input: text)
       call_menu
     end
   end
@@ -43,11 +45,14 @@ class ButtonReceiver
   def add_subject
     BotOptions.instance.edit_message(text: 'update_document_subject')
     message = BotOptions.instance.get_single_input
+    p message
     message_text = message.caption
     message_id = message.message_id
+    raise 'Incorrect input' if message.caption.nil? || message.document.nil?
+
     FileConfigDb.instance.set_subject(subject: message_text, message_id: message_id)
   rescue StandardError => e
-    p e
+    ErrorLogDb.instance.log_error(level: inspect + '=>' + caller[0][/`.*'/][1..-2], message: message, exception: e.inspect)
     BotOptions.instance.send_message(text: 'update_document_subject_failed')
   else
     BotOptions.instance.send_message(text: 'update_document_subject_succeed')
@@ -57,14 +62,15 @@ class ButtonReceiver
 
   def edit_subject
     subject = FileConfigDb.instance.get_subjects
-    markup, available_buttons = ReceiverHelper.markup_string(subject)
+    markup, available_buttons = ReceiverButtonHelper.markup_string(subject)
     subject.map(&:reverse!)
-    display_string = ReceiverHelper.display_string(subject)
+    display_string = ReceiverButtonHelper.display_string(subject)
     BotOptions.instance.send_message(text: 'update_document_edit_showlist', additional_text: display_string, markup: markup)
     to_edit = BotOptions.instance.get_single_input.text
     if available_buttons.flatten.include?(to_edit)
       unless FileConfigDb.instance.delete_subject(to_edit)
-        BotOptions.instance.send_message(text: 'update_document_subject_failed')
+        markup = MakeInlineMarkup.delete_board
+        BotOptions.instance.send_message(text: 'update_document_subject_failed', markup: markup)
         return
       end
       markup = MakeInlineMarkup.delete_board
@@ -72,7 +78,8 @@ class ButtonReceiver
       sleep(1)
       call_menu
     else
-      BotOptions.instance.send_message(text: 'update_document_subject_failed')
+      markup = MakeInlineMarkup.delete_board
+      BotOptions.instance.send_message(text: 'update_document_subject_failed', markup: markup)
     end
   end
 
