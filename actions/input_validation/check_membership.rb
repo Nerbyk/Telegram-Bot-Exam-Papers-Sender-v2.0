fd288538a297d7bfebfad26fdcae8020c4fe0699
nerbyk <danil.maximov2000@gmail.com>
2020-06-15 13:59:19 +0200

    membersip validation class(vk + telegram)

---
+++ actions/input_validation/check_membership.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'vkontakte_api'
+require 'uri'
+class CheckMembership
+  def initialize(link_path:)
+    @link_path    = link_path
+    @token        = ENV['VK_TOKEN']
+    @version      = ENV['VK_API_VERSION']
+    @vk           = VkontakteApi::Client.new(token)
+    @group_id     = get_vk_path
+    @community_id = get_telegram_path
+  end
+
+  def to_validate
+    link_path = get_numeric_user_id
+    vk_status = vk.groups.isMember('v' => version, 'group_id' => group_id, 'user_id' => link_path.to_i)
+    vk_status = vk_status == 1
+    # telegram_status = BotOptions.instance.bot.getChatMember(chat_id: '@' + community_id, user_id: BotOptions.instance.message.from.id)
+    # telegram_status['result']['status']
+    telegram_status = true
+    vk_status == telegram_status
+  end
+
+  private
+
+  def get_vk_path
+    link = URI.parse(CfgConst::Links.instance.vk)
+    link.path.delete('/')
+  end
+
+  def get_telegram_path
+    link = URI.parse(CfgConst::Links.instance.telegram)
+    link.path.delete('/')
+  end
+
+  def get_numeric_user_id
+    if link_path.include?('id')
+      link_path.slice!('id')
+    else
+      numeric_id = vk.users.get("v": version, "user_ids": link_path, "fields": 'id')
+      numeric_id[0]['id']
+    end
+  end
+
+  def is_int?
+    true if Int(@link)
+  rescue StandardError
+    false
+  end
+
+  attr_reader :link_path, :token, :version, :vk, :group_id, :community_id
+end
