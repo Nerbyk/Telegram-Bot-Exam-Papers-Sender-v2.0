55860ad85158c8fcbfcc589d993079f515b278d2
nerbyk <danil.maximov2000@gmail.com>
2020-06-16 19:58:15 +0200

    singleton BotOptions => module BotOptions

--- bot_options.rb
+++ bot_options.rb
@@ -2,16 +2,7 @@
 
 require 'singleton'
 
-class BotOptions
-  attr_accessor :bot, :message, :role, :my_text
-  include Singleton
-  def initialize
-    @bot = bot
-    @message = message
-    @role = role
-    @my_text = my_text
-  end
-
+module BotOptions
   def send_message(text:, markup: nil, additional_text: '')
     bot.api.send_message(chat_id: message.from.id,
                          text: my_text.reply(text) + additional_text,
@@ -32,8 +23,6 @@ class BotOptions
                               message_id: message.message.message_id,
                               text: my_text.reply(text) + additional_text,
                               reply_markup: markup)
-  rescue StandardError
-    send_message(text: 'admin_exception_error')
   end
 
   def get_single_input
