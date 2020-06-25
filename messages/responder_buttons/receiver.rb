68a991585b1642f636c1d898e9343659c55904bf
nerbyk <danil.maximov2000@gmail.com>
2020-06-25 21:26:57 +0200

    changed structure

--- messages/responder_buttons/receiver.rb
+++ messages/responder_buttons/receiver.rb
@@ -15,40 +15,15 @@ class ButtonReceiver
   # admin buttons
   def add_admin
     edit_message(text: 'manage_admins_add')
-    begin
-      input = get_single_input
-      text = input.text.split(' ')
-      ReceiverButtonHelper.check_db_string(text) ? true : raise('Input check failed')
-    rescue Exception => e
-      Db::ErrorLog.instance.log_error(level: inspect + '=>' + caller[0][/`.*'/][1..-2], message: input, exception: e.inspect)
-      send_message(text: 'manage_admins_error')
-    else
-      Db::UserConfig.instance.add_admin(input: text)
-      call_menu
-    end
+    Db::UserConfig.instance.set_status(status: CfgConst::AdminStatus::ADD_ADMIN, user_id: message.from.id.to_s)
   end
 
   def delete_admin
     db_data = Db::UserConfig.instance.get_admins
     display_string = ReceiverButtonHelper.display_string(db_data)
     markup, available_buttons = ReceiverButtonHelper.markup_string(db_data)
-
     send_message(text: 'manage_admins_delete', markup: markup, additional_text: display_string)
-    to_delete = get_single_input.text
-    if available_buttons.flatten.include?(to_delete)
-      unless Db::UserConfig.instance.delete_admin(to_delete)
-        send_message(text: 'manage_admins_error')
-        return
-      end
-      markup = MakeInlineMarkup.delete_board
-      send_message(text: 'manage_admins_deleted', markup: markup)
-      call_menu
-    else
-      send_message(text: 'manage_admins_error')
-    end
-    # TODO: After including inspect_request block
-    # change state, if a request is inspecting by this admin
-    # while deleting permissions + send notification about rights
+    Db::UserConfig.instance.set_status(status: CfgConst::AdminStatus::DELETE_ADMIN, user_id: message.from.id.to_s)
   end
 
   def add_subject
