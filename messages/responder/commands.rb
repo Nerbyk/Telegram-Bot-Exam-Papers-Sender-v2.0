3d7e7d002783c22438b1ad33dbaa2f11318e62d8
nerbyk <danil.maximov2000@gmail.com>
2020-06-30 20:27:44 +0200

    new command for user form filling

--- messages/responder/commands.rb
+++ messages/responder/commands.rb
@@ -58,28 +58,34 @@ class DeleteAdminAction < Command
   end
 end
 
-class AddSubjectAction < Command 
-  def execute 
+class AddSubjectAction < Command
+  def execute
     request.add_subject_action
   end
 end
 
-class DeleteSubjectAction < Command 
-  def execute 
+class DeleteSubjectAction < Command
+  def execute
     request.delete_subject_action
   end
 end
 
-class UpdatLinkAction < Command 
-  def execute 
-    request.update_link_action 
+class UpdatLinkAction < Command
+  def execute
+    request.update_link_action
   end
 end
 
-class SetAlertAction < Command 
-  def execute 
+class SetAlertAction < Command
+  def execute
     request.set_alert_action
   end
 end
 
 # User Commands
+
+class FormFillingAction < Command
+  def execute
+    request.user_form_filling
+  end
+end
