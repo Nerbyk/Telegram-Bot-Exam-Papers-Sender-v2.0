af2f327c3ff3ad625139a0ae68b5188eda927142
nerbyk <danil.maximov2000@gmail.com>
2020-07-01 20:51:19 +0200

    new methods to check,errlog only inside this class

--- actions/input_validation/check_input.rb
+++ actions/input_validation/check_input.rb
@@ -5,10 +5,18 @@ require './actions/input_validation/check_membership.rb'
 class CheckUserInput
   MAX_SUBJECTS = 6 # more than 6 subjects cannot be assigned to students
   def self.name(input:)
-    input.split(' ').length == 2
+    if input.split(' ').length == 2
+      true
+    else
+      raise 'Incorrect input format'
+    end
+  rescue Exception => e
+    to_log(level: inspect, exception: e, user_input: input)
+    # Db::ErrorLog.instance.log_error(level: inspect + '=>' + caller[0][/`.*'/][1..-2], message: input, exception: e.inspect)
+    false
   end
 
-  def self.link(input:)
+  def self.link(input:, options:)
     link = input.text
     link = 'https://' + link unless link.include?('https://') || link.include?('http://')
     raise "Incorrect Link foprmat(link.include?('vk.com'))" unless link.include?('vk.com')
@@ -16,30 +24,46 @@ class CheckUserInput
     link = URI.parse(link)
     raise "Incorrect Link foprmat(link.path.count('/') != 1)" unless link.path.count('/') == 1
 
-    validation = CheckMembership.new(link_path: link.path.delete('/'))
+    validation = CheckMembership.new(link_path: link.path.delete('/'), options: options)
     raise 'Membership check failed' unless validation.to_validate
 
     true
   rescue Exception => e
-    Db::ErrorLog.instance.log_error(level: inspect + '=>' + caller[0][/`.*'/][1..-2], message: input, exception: e.inspect)
+    to_log(level: inspect, exception: e, user_input: input)
     false
   end
 
-  def self.single_subject(subject:, available_list:)
-    available_list << CfgConst::BotCommands::START
-    available_list.include?(subject.text) ? true : raise('Unexpected input')
+  def self.single_subject(input:, available_list:)
+    available_list << CfgConst::BotCommands::STOP
+    available_list.include?(input.text) ? true : raise('Unexpected input')
   rescue Exception => e
-    Db::ErrorLog.instance.log_error(level: inspect + '=>' + caller[0][/`.*'/][1..-2], message: subject, exception: e.inspect)
+    to_log(level: inspect, exception: e, user_input: input)
     false
   end
 
-  def self.all_subjects(subjects:)
-    if subjects.length > MAX_SUBJECTS || subjects.length > subjects.uniq.length
-      raise
+  def self.all_subjects(input:)
+    if input.length > MAX_SUBJECTS || input.length > input.uniq.length
+      raise 'Single subject check failed'
     else
       true
     end
-  rescue Exception
+  rescue Exception => e
+    to_log(level: inspect, exception: e, user_input: nil)
     false
+  end
+
+  def self.check_photo_format(input:)
+    if input.photo == []
+      raise 'Incorrect message format'
+    else
+      true
+    end
+  rescue Exception => e
+    to_log(level: inspect, exception: e, user_input: input)
+    false
+  end
+
+  def self.to_log(level:, exception:, user_input:)
+    Db::ErrorLog.instance.log_error(level: level + '=>' + caller[0][/`.*'/][1..-2], message: user_input, exception: exception.inspect)
   end
 end
