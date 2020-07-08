ecc9df6de19f271f7fa9dfa191d11791b8e3e08a
nerbyk <danil.maximov2000@gmail.com>
2020-07-08 20:36:40 +0200

    telegraph articles to warn about matches

---
+++ actions/telegrpah_article.rb
@@ -0,0 +1,33 @@
+require 'telegraph-ruby'
+
+class GenerateArticleLink
+  def initialize(*matched_requests)
+    @matched_requests = matched_requests
+    @telegraph = Telegraph.new
+    @token = telegraph.createAccount(short_name: 'PozorBot')['access_token']
+  end
+
+  def create_article
+    return_links = matched_requests.map do |request|
+      user_id = request[:user_id]
+      user_name = request[:name]
+      user_link = request[:link]
+      request_data = request[:created].to_s
+      user_subjects = request[:subjects].gsub(';', ' ')
+      user_photo = "https://api.telegram.org/file/bot#{ENV['TOKEN']}/#{request[:photo].split(';').last}"
+      page = telegraph.createPage(access_token: token,
+                                  title: "Заявка №#{user_id}",
+                                  content: "[{\"tag\":\"figure\",
+                                \"children\":[
+                                {\"tag\":\"img\",\"attrs\":{\"src\":\"#{user_photo}\"}},
+                                {\"tag\":\"figcaption\",\"children\":[\"\"]}]},
+                                {\"tag\":\"p\", \"children\":[\"Дата Подачи: #{request_data}\nИмя и Фамилия: #{user_name}\nСсылка ВК: #{user_link}\nПредметы: #{user_subjects}\"]}]")
+      page['url']
+    end
+    return_links
+  end
+
+  private
+
+  attr_reader :matched_requests, :token, :telegraph
+end
