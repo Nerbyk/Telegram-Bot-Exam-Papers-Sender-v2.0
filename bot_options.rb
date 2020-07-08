538ed86ae82a10c0bc4faa5eb1b13ccb6d319b40
nerbyk <danil.maximov2000@gmail.com>
2020-07-08 20:35:58 +0200

    methods to send messages with parse mode

--- bot_options.rb
+++ bot_options.rb
@@ -17,6 +17,13 @@ module BotActions
                          reply_markup: markup)
   end
 
+  def send_message_parse_mode(chat_id: message.from.id, text:, markup: nil)
+    bot.api.send_message(chat_id: chat_id,
+                         text: text,
+                         reply_markup: markup,
+                         parse_mode: 'HTML')
+  end
+
   def forward_message(chat_id: message.from.id, from_chat_id: message.from.id, message_id:)
     bot.api.forward_message(chat_id: chat_id, from_chat_id: from_chat_id, message_id: message_id)
   end
@@ -38,5 +45,13 @@ module BotActions
                        photo: photo,
                        caption: my_text.reply(text) + additional_text,
                        reply_markup: markup)
+  end
+
+  def send_photo_parse_mode(text: nil, markup: nil, photo: nil)
+    bot.api.send_photo(chat_id: message.from.id,
+                       photo: photo,
+                       caption: text,
+                       reply_markup: markup,
+                       parse_mode: 'HTML')
   end
 end
