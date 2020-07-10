70513f11639e2b5326c316e0c4f0be57c57fa5cc
nerbyk <danil.maximov2000@gmail.com>
2020-07-10 18:48:09 +0200

    type error fixed

--- services/req_amount_observer.rb
+++ services/req_amount_observer.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 
 class Notifier
+  attr_reader :bot
   def update
     puts "Logged #{AmountOfRequests.instance.current} uninspected requests."
-    invoke_admin if AmountOfRequests.instance.current >= AmountOfRequests.instance.target
+    invoke_admin if AmountOfRequests.instance.current.to_i >= AmountOfRequests.instance.target.to_i
   end
 
   private
 
   include BotActions
   def invoke_admin
+    @bot = BotOptions.instance.bot
     get_admins.each do |admin_id|
       # send_message(chat_id: admin_id, text: 'notification_requests', additional_text: AmountOfRequests.instance.current)
       puts "chat_id: #{admin_id}, text: 'notification_requests', additional_text: #{AmountOfRequests.instance.current}"
+      send_message_parse_mode(chat_id: admin_id.to_i, text: "Кол-во непроверенных заявок - <b>#{AmountOfRequests.instance.current}</b>\n /inspect_nostr - для проверки")
     end
   end
 
   def get_admins
-    admins = Db::User.instance.get_admins << [12_345, 'NameNick']
+    admins = Db::User.instance.get_admins
     admins = admins.map(&:first)
     admins << ENV['ADMIN_ID']
   end
