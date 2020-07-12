3de053091ba645c0c93530540998b246834136b8
nerbyk <danil.maximov2000@gmail.com>
2020-07-12 16:09:57 +0200

    block to enable maintenance mode at runtime

--- bot.rb
+++ bot.rb
@@ -16,16 +16,38 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   BotOptions.instance.bot = bot
   Notifier.about_restart
   bot.listen do |message|
-    options = { bot: bot, message: message }
-  begin
-     case message
-     when Telegram::Bot::Types::CallbackQuery
-       ButtonResponder.new(options: options).respond
-     else
-       MessageResponder.new(options: options).respond if message.chat.type != 'channel' # restrict access to channels
-     end
-  rescue StandardError => exception
-    Db::ErrorLog.instance.log_error(level: 'high' + '=>' + caller[0][/`.*'/][1..-2], message: message, exception: exception.inspect)
-  end
+  include BotOptions
+    if Config::Access.instance.user
+      options = { bot: bot, message: message }
+      begin
+        case message
+        when Telegram::Bot::Types::CallbackQuery
+          ButtonResponder.new(options: options).respond
+        else
+          MessageResponder.new(options: options).respond if message.chat.type != 'channel' # restrict access to channels
+        end
+      rescue StandardError => exception
+        send_message(text: 'exception_message')
+        Db::ErrorLog.instance.log_error(level: 'high' + '=>' + caller[0][/`.*'/][1..-2], message: message, exception: exception.inspect)
+      end
+    else  
+      if message.from.id == ENV['ADMIN_ID'] || message.from.id == ENV['DEV_ID']
+        options = { bot: bot, message: message }
+        begin
+          case message
+          when Telegram::Bot::Types::CallbackQuery
+            ButtonResponder.new(options: options).respond
+          else
+            MessageResponder.new(options: options).respond if message.chat.type != 'channel' # restrict access to channels
+          end
+        rescue StandardError => exception
+          send_message(text: 'exception_message')
+          Db::ErrorLog.instance.log_error(level: 'high' + '=>' + caller[0][/`.*'/][1..-2], message: message, exception: exception.inspect)
+        end
+      else  
+        send_message(text: 'maintenance')
+      end
+    end
+    
   end
 end
