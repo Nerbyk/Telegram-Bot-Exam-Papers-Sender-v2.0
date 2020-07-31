72aff5a948a9982321b913a9269c2e57a6ff7f4f
Danil Maksimov <57483156+Nerbyk@users.noreply.github.com>
2020-07-31 22:12:46 +0200

    Update developer_commands.rb

--- messages/responder/receiver_modules/developer_commands.rb
+++ messages/responder/receiver_modules/developer_commands.rb
@@ -35,8 +35,9 @@ module DeveloperCommands
     user_request = Db::UserMessage.instance.get_user_data(user_id: user_id.to_i)
     request_text = "\tЗаявка №#{user_request[:user_id]}\nTG ID: <a href=\"tg://user?id=#{user_request[:user_id]}\">#{user_request[:user_id]}</a>\nИмя и Фамилия: #{user_request[:name]}\nСсылка ВК: #{user_request[:link]}\nПредметы: #{user_request[:subjects].gsub(';', ' ')}"
     send_photo_parse_mode(photo: user_request[:photo].split(';').first, text: request_text)
-  rescue StandardError => e
-    exception_dev(e)
+  rescue StandardError # => e
+   # exception_dev(e)
+    send_message_parse_mode(text: "User <a href=\"tg://user?id=#{user_id}\">#{user_id}</a>\nData: #{user_request}")
   end
 
   def ban_dev
